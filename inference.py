b43b57aff52dd8695bbc10f99955e44975478b2c
Thomas Dürr <thomduerr@gmail.com>
2022-03-16 22:22:21 +0100

    remove seed

--- inference.py
+++ inference.py
@@ -53,9 +53,6 @@ def inference(args):
     ])
 
     print('[INFO] loading the test dataset ...')
-    print(args['seed'])
-    if args['seed']:
-        torch.manual_seed(args['seed'])
     test_dataset = datasets.ImageFolder(root=args['dataset_path'], transform=test_transforms)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=os.cpu_count(), pin_memory=True if config.DEVICE == "cuda" else False)
 
@@ -120,7 +117,6 @@ if __name__ == '__main__':
     parser.add_argument('--image-path', type=pathlib.Path, help='path to test images instead of batch')
     parser.add_argument('--show-metrics', type=bool, default=True, help='print inference metrics')
     parser.add_argument('--batch', type=int, default=config.PRED_BATCH_SIZE, help='batch size')
-    parser.add_argument('--seed', type=int, default=None, help='seed for randomizing data-loader (default: None)')
     args = vars(parser.parse_args())
 
     inference(args)
